644066065231487c59b094b9dfa8cd6261453133
Ankit Choudhary <99082061+BLUEhorizon28@users.noreply.github.com>
2026-03-31 01:00:37 +0530

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script 1: System Identity Report
+
+echo "================================"
+echo " Open Source Audit — Ankit"
+echo "================================"
+
+echo "Kernel  : $(uname -r)"
+echo "User    : $(whoami)"
+echo "Home    : $HOME"
+echo "Uptime  : $(uptime -p)"
+echo "Date    : $(date)"
+
+echo "Distro  : $(lsb_release -d | cut -f2)"
+
+echo "License : GNU GPL (Linux Kernel)"
